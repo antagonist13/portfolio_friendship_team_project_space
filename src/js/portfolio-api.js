5ca260de232a851047bede5bf17f6b953cbc959e
Vadim Korol <55877443+korolvv@users.noreply.github.com>
2024-04-11 14:47:49 +0300

    receiving info from api

--- src/js/portfolio-api.js
+++ src/js/portfolio-api.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 export async function getData() {
-const BASE_URL = 'https://portfolio-js.b.goit.study';
-const END_POINT = '/api/reviews';
-    const url = `${BASE_URL}${END_POINT}`;
-    const res = await axios.get(url)
-    return res.data
+  const BASE_URL = 'https://portfolio-js.b.goit.study';
+  const END_POINT = '/api/reviews';
+  const url = `${BASE_URL}${END_POINT}`;
+  const res = await axios.get(url);
+  return res.data;
 }
 
 export async function postData(arr) {
-const BASE_URL = 'https://portfolio-js.b.goit.study';
-const END_POINT = '/api/requests';
-const url = `${BASE_URL}${END_POINT}`;
-    const res = await axios.post(url, arr);
-    return res;
+  const BASE_URL = 'https://portfolio-js.b.goit.study';
+  const END_POINT = '/api/requests';
+  const url = `${BASE_URL}${END_POINT}`;
+  const res = await axios.post(url, arr);
+  return res;
 }
 
 // Приклад запиту для: postData
--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,9 +1,47 @@
 import Swiper from 'swiper';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 import 'swiper/css';
 import { Keyboard } from 'swiper/modules';
+import { getData } from './portfolio-api';
 
 const btnPrev = document.querySelector('.swiper-button-prev');
 const btnNext = document.querySelector('.swiper-button-next');
+const reviewsList = document.querySelector('.reviews__list');
+
+document.addEventListener('DOMContentLoaded', () => {
+  let result;
+  getData()
+    .then(data => {
+      result = data
+        .map(item => {
+          return `
+      <li class="reviews__item swiper-slide">
+      <img
+        width="48"
+        class="reviews__img"
+        src="${item.avatar_url}"
+        alt="${item.author}"
+      />
+      <h3 class="reviews__h3">${item.author}</h3>
+      <p class="reviews__descr">${item.review}</p>
+    </li>`;
+        })
+        .join('');
+      reviewsList.insertAdjacentHTML('beforeend', result);
+    })
+    .catch(error => {
+      reviewsList.insertAdjacentHTML(
+        'beforeend',
+        `<p class="non_info">NOT FOUND</p>`
+      );
+      iziToast.error({
+        position: 'topRight',
+        message: 'Sorry, there are no reviews. Please reload your page!',
+      });
+      console.log(error);
+    });
+});
 
 const swiper = new Swiper('.swiper', {
   modules: [Keyboard],
@@ -28,9 +66,6 @@ btnNext.addEventListener('click', () => {
   }
 });
 
-// swiper.on('activeIndexChange', () => {
-//   btnPrev.classList.remove('disabled_btn');
-// });
 swiper.on('reachBeginning', () => {
   btnPrev.classList.add('disabled_btn');
   btnNext.classList.remove('disabled_btn');
