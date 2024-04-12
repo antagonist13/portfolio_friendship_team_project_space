90ae28361b32391596f50571323a72b6b0ee6e66
DmytroRev <149614107+DmytroRev@users.noreply.github.com>
2024-04-12 20:30:09 +0300

    upgrade descktop layot

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,8 +1,8 @@
-// import Accordion from 'accordion-js';
-// import 'accordion-js/dist/accordion.min.css';
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 
-// new Accordion('.accordion-container', {
-//   duration: 400,
-//   showMultiple: true,
-// });
+new Accordion('.accordion-container', {
+  duration: 400,
+  showMultiple: true,
+});
 
