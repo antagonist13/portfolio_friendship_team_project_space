05d1b61a05d82c94267990150c31a515a46c0341
DmytroRev <149614107+DmytroRev@users.noreply.github.com>
2024-04-13 13:50:48 +0300

    finish project

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,49 +1,49 @@
-// import Swiper from 'swiper';
-// import 'swiper/css';
-// import { Keyboard } from 'swiper/modules';
+import Swiper from 'swiper';
+import 'swiper/css';
+import { Keyboard } from 'swiper/modules';
 
-// const prodPrev = document.querySelector(".swiper-button-prev")
-// const prodNext = document.querySelector(".swiper-button-next")
-
-
-
-// const swiper = new Swiper('.swiper', {
-//     direction: 'horizontal',
-//   modules: [Keyboard],
-//   keyboard: {
-//     enabled: true,
-//     onlyInViewport: false,
-//   },
-//   speed: 400,
-//   spaceBetween: 100,
-// });
+const prodPrev = document.querySelector(".project-swiper-button-prev")
+const prodNext = document.querySelector(".project-swiper-button-next")
 
 
 
-// prodPrev.addEventListener("click", () => {
-//     swiper.slidePrev().hideOnClick;
-//     if (prodNext.classList.contains("project-btn-swap-disable")) {
-//         prodNext.classList.remove("project-btn-swap-disable")
-//     }
-// })
-
-// prodNext.addEventListener("click", () => {
-//     swiper.slideNext();
-//     if (prodPrev.classList.contains("project-btn-swap-disable")) {
-//         prodPrev.classList.remove("project-btn-swap-disable")
-//     }
-// })
+const swiper = new Swiper('.project-swiper', {
+    direction: 'horizontal',
+  modules: [Keyboard],
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  speed: 400,
+  spaceBetween: 100,
+});
 
 
-// swiper.on('reachBeginning', () => {
-//     prodPrev.classList.add('project-btn-swap-disable');
-//     prodNext.classList.remove('project-btn-swap-disable');
-//   });
 
-//   swiper.on('reachEnd', () => {
-//   prodNext.classList.add('project-btn-swap-disable');
-//   prodPrev.classList.remove('project-btn-swap-disable');
-// });
+prodPrev.addEventListener("click", () => {
+    swiper.slidePrev().hideOnClick;
+    if (prodNext.classList.contains("project-btn-swap-disable")) {
+        prodNext.classList.remove("project-btn-swap-disable")
+    }
+})
+
+prodNext.addEventListener("click", () => {
+    swiper.slideNext();
+    if (prodPrev.classList.contains("project-btn-swap-disable")) {
+        prodPrev.classList.remove("project-btn-swap-disable")
+    }
+})
+
+
+swiper.on('reachBeginning', () => {
+    prodPrev.classList.add('project-btn-swap-disable');
+    prodNext.classList.remove('project-btn-swap-disable');
+  });
+
+  swiper.on('reachEnd', () => {
+  prodNext.classList.add('project-btn-swap-disable');
+  prodPrev.classList.remove('project-btn-swap-disable');
+});
 
 
 
