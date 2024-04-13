e7f8224bb14106a29bba52432ebb64d7d92a06b1
Alina Moiseichuk <alinamoyseychuk8@gmail.com>
2024-04-13 14:17:53 +0200

    css desktop

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,11 +1,3 @@
-// import Accordion from 'accordion-js';
-// import 'accordion-js/dist/accordion.min.css';
-
-// new Accordion('.accordion-container', {
-//   duration: 400,
-//   showMultiple: true,
-// });
-
 const faqs = document.querySelectorAll(".faq-accordion");
 
 faqs.forEach(faq => {
@@ -13,5 +5,3 @@ faqs.forEach(faq => {
         faq.classList.toggle("active");
     });
 });
-
-
